84442d4daadca58fbbd99cfd5cf7a756bb445a39
Isaac Greene <irgreene@icloud.com>
2024-07-01 17:03:46 -0400

    Initial Commit

---
+++ Scores/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scores
+//
+//  Created by Isaac Greene on 2024-07-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scores/ScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoresApp.swift
+//  Scores
+//
+//  Created by Isaac Greene on 2024-07-01.
+//
+
+import SwiftUI
+
+@main
+struct ScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
