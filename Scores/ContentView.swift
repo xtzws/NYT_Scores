4244343e0908b29268ce70b64c25965178e9d305
xtzws <ig@isaac.run>
2025-03-23 20:50:03 -0400

    Added app, and icon

--- Scores/ContentView.swift
+++ Scores/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+struct LineScore: Identifiable {
+    let id = UUID()
+    var score: String
+}
+
 struct ContentView: View {
+    @State var lineScores: [LineScore] = Array(repeating: LineScore(score: ""), count: 4)
+    
+    let allOptions = ["NO SOLVE", "🟪🟪🟪🟪", "🟦🟦🟦🟦", "🟩🟩🟩🟩", "🟨🟨🟨🟨"]
+    let scoreMapping: [String: Int] = [
+        "NO SOLVE": 0,
+        "🟪🟪🟪🟪": 4,
+        "🟦🟦🟦🟦": 3,
+        "🟩🟩🟩🟩": 2,
+        "🟨🟨🟨🟨": 1
+    ]
+    @State var scores = [0, 0, 0, 0]
+    let weights = [4, 3, 2, 1]
+    
+    var totalScore: Int {
+        zip(scores, weights).reduce(0) { $0 + scoreMapping[allOptions[$1.0]]! * $1.1 }
+    }
+    
+    var availableOptions: [[String]] {
+        var selectedOptions = Set<String>()
+        return scores.map { score in
+            let selectedOption = allOptions[score]
+            selectedOptions.insert(selectedOption)
+            return allOptions.filter { !selectedOptions.contains($0) || $0 == selectedOption || $0 == "NO SOLVE"}
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(0..<scores.count, id: \.self) { index in
+                HStack {
+                    Text("LINE \(index+1)")
+                    Picker("SCORE", selection: Binding(
+                        get: { scores[index] },
+                        set: { newValue in
+                            scores[index] = newValue
+                            lineScores[index].score = allOptions[newValue]
+                        }
+                    )) {
+                        ForEach(availableOptions[index], id: \.self) { option in
+                            Text(option).tag(allOptions.firstIndex(of: option)!)
+                        }
+                    }
+                }
+            }
+            Text("Score: \(totalScore)/30")
+                .padding()
         }
-        .padding()
+        .onChange(of: scores) { newScores in
+            for (index, newValue) in newScores.enumerated() {
+                lineScores[index].score = allOptions[newValue]
+            }
+        }
     }
 }
 
